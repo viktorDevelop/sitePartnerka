6ef5f57ff628308ff8edcefb88cd05366f726113
viktor <viktor@mail.ru>
2020-02-18 12:05:18 +0300

    Category controller class

---
+++ app/controllers/CategoryController.php
@@ -0,0 +1,40 @@
+<?
+namespace app\controllers;
+use app\controllers\FrontController;
+
+/**
+ * 
+ */
+class Category extends FrontController
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+
+	public function actionIndex($value='')
+	{
+		# code...
+	}
+
+	public function actionListArticle($value='')
+	{
+		# code...
+	}
+
+	public function actionAdd($value='')
+	{
+		# code...
+	}
+
+	public function actionEdit($value='')
+	{
+		# code...
+	}
+
+	public function actionDelete($value='')
+	{
+		# code...
+	}
+}
