3e3f6c8d746d9905c921e831ae02255d23ec1f0b
viktor <viktor@mail.ru>
2020-02-19 09:23:21 +0300

    create parser controller class

---
+++ app/controllers/ParserController.php
@@ -0,0 +1,16 @@
+<?
+namespace app\controllers;
+use app\controllers\FrontController;
+
+
+class ParserController extends FrontController
+{
+	
+	 
+//http://site.ru/parser/
+	public function actionIndex($value='')
+	{
+		 
+	}
+ 
+}
