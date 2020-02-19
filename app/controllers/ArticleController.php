9f438f52fa91ad4d0d7b38e3aa3f40e75069f14c
viktor <viktor@mail.ru>
2020-02-19 09:21:12 +0300

    create Article controller class and actions

---
+++ app/controllers/ArticleController.php
@@ -0,0 +1,46 @@
+<?
+namespace app\controllers;
+use app\controllers\FrontController;
+
+/**
+ * 
+ */
+class ArticleController extends FrontController
+{
+	
+	 
+//http://site.ru/
+	public function actionIndex($value='')
+	{
+		# code...
+	}
+//http://site.ru/article/php-title
+	public function actionArticle($value='')
+	{
+		 
+	}
+
+//http://site.ru/admin/article/php
+	public function actionListArticle($value='')
+	{
+		 
+	}
+
+//http://site.ru/admin/article/add
+
+	public function actionAdd()
+	{
+		# code...
+	}
+//http://site.ru/admin/article/edite
+
+	public function actionEdit()
+	{
+		 
+	}
+//http://site.ru/admin/article/delete
+	public function actionDelete()
+	{ 
+		 
+	}
+}
