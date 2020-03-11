c483674469c9a980c9c3d8d0ed83b1a6737e2d8e
viktor <vk.job.00@mail.ru>
2020-03-11 10:24:24 +0300

    create class templateBlog

--- app/libs/TemplateBlog.php
+++ app/libs/TemplateBlog.php
@@ -9,6 +9,7 @@ class TemplateBlog
 	private $view;
 	public $dataView = [];
 	private $classLoad;
+	private $pages = "index";
 
 	function __construct()
 	{
@@ -22,7 +23,11 @@ class TemplateBlog
 	{
 		$this->view->asideMenu =  $this->getDataMenuAside();
 		$this->view->view =  $this->getDataView();
-		 $this->view->load('blog/index.php'); 
+		$this->view->title = $this->getHead('title');
+		$this->view->keyword = $this->getHead('keyword');
+		$this->view->description = $this->getHead('description');
+
+		$this->view->load('blog/index.php'); 
 	}
 
 
@@ -33,7 +38,7 @@ class TemplateBlog
 		 $this->view->arResult = $this->dataView;
 		 $this->view->error = "Данная страница была удалена или  у вас нет прав";
 		 
-		return 	$this->view->render('/blog/pages/index_view.php');
+		return 	$this->view->render('/blog/pages/'.$this->pages.'_view.php');
 	}
 
 	private function getDataMenuAside()
@@ -44,6 +49,17 @@ class TemplateBlog
 			return 	$this->view->render('/blog/blocks/asideMenu.php');
  
 		}
+	public function SetPages($value='')
+	{
+		 if (!empty($value)) {
+		 		$this->pages = $value;
+		 }
+	}
+
+	private function getHead($value)
+	{
+		 
+	}
 
 }
  
